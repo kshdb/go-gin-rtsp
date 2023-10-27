1e6c27c6f272b5a04e10a630b84e38e16bf46736
sxh <123456>
2023-10-27 15:36:41 +0800

    首次提交

---
+++ Dockerfile
@@ -0,0 +1,40 @@
+FROM ubuntu:18.04 as builder
+
+ENV APP_DIR=/go/src
+ENV GO_VERSION=1.13.3
+ENV GO111MODULE=on
+ENV GOPROXY=https://goproxy.io
+
+COPY . ${APP_DIR}
+WORKDIR ${APP_DIR}
+
+RUN sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list \
+    && sed -i 's/security.ubuntu.com/mirrors.aliyun.com/g'  /etc/apt/sources.list \
+    && apt-get update \
+    && apt-get install -y wget\
+    && wget -nv https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz \
+    && tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz \
+    && export PATH=$PATH:/usr/local/go/bin \
+    && go build -o gin-rtsp
+
+
+FROM ubuntu:18.04
+
+ENV TZ=Asia/Shanghai
+ENV LANG=en_US.UTF-8
+ENV LOG_LEVEL=info
+
+RUN sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list \
+    && sed -i 's/security.ubuntu.com/mirrors.aliyun.com/g'  /etc/apt/sources.list \
+    && apt-get update \
+    && ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone \
+    && apt-get install -y locales tzdata lib \
+    && localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8 \
+    && apt-get clean && apt-get autoclean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+
+COPY --from=builder /go/src/gin-rtsp /usr/local/bin/gin-rtsp
+
+EXPOSE 3000
+
+ENTRYPOINT [ "/usr/local/bin/gin-rtsp" ]
---
+++ service/rtsptrans.go
@@ -0,0 +1,119 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"ginrtsp/serializer"
+	"ginrtsp/util"
+	"io"
+	"os/exec"
+	"strings"
+	"sync"
+
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+// RTSPTransSrv RTSP 转换服务 struct
+type RTSPTransSrv struct {
+	URL string `form:"url" json:"url" binding:"required,min=1"`
+}
+
+// processMap FFMPEG 进程刷新通道，未在指定时间刷新的流将会被关闭
+var processMap sync.Map
+
+// Service RTSP 转换服务
+func (service *RTSPTransSrv) Service() *serializer.Response {
+	simpleString := strings.Replace(service.URL, "//", "/", 1)
+	splitList := strings.Split(simpleString, "/")
+
+	if splitList[0] != "rtsp:" && len(splitList) < 2 {
+		return &serializer.Response{
+			Code: 400,
+			Msg:  "不是有效的 RTSP 地址",
+		}
+	}
+
+	// 多个客户端需要播放相同的RTSP流地址时，保证返回WebSocket地址相同
+	processCh := uuid.NewV3(uuid.NamespaceURL, splitList[1]).String()
+	if ch, ok := processMap.Load(processCh); ok {
+		*ch.(*chan int) <- 1
+	} else {
+		reflush := make(chan int)
+		if cmd, stdin, err := runFFMPEG(service.URL, processCh); err != nil {
+			return serializer.Err(400, err.Error(), err)
+		} else {
+			go keepFFMPEG(cmd, stdin, &reflush, processCh)
+		}
+	}
+
+	playURL := fmt.Sprintf("/stream/live/%s", processCh)
+	return serializer.BuildRTSPPlayPathResponse(playURL)
+}
+
+func keepFFMPEG(cmd *exec.Cmd, stdin io.WriteCloser, ch *chan int, playCh string) {
+	processMap.Store(playCh, ch)
+	defer func() {
+		processMap.Delete(playCh)
+		_ = stdin.Close()
+		util.Log().Info("Stop translate rtsp id %v", playCh)
+	}()
+
+	for {
+		select {
+		case <-*ch:
+			util.Log().Info("Reflush channel %s", playCh)
+
+		case <-time.After(6000 * time.Minute):
+			_, _ = stdin.Write([]byte("q"))
+			err := cmd.Wait()
+			if err != nil {
+				util.Log().Error("Run lib err %v", err.Error())
+			}
+			return
+		}
+	}
+}
+
+func runFFMPEG(rtsp string, playCh string) (*exec.Cmd, io.WriteCloser, error) {
+	params := []string{
+		"-rtsp_transport",
+		"tcp",
+		"-re",
+		"-timeout",
+		"5",
+		"-i",
+		rtsp,
+		"-q",
+		"5",
+		"-f",
+		"mpegts",
+		"-fflags",
+		"nobuffer",
+		"-c:v",
+		"mpeg1video",
+		"-an",
+		"-s",
+		"960x540",
+		fmt.Sprintf("http://127.0.0.1:3000/stream/upload/%s", playCh),
+	}
+
+	util.Log().Debug("FFmpeg cmd: ffmpeg %v", strings.Join(params, " "))
+	cmd := exec.Command("./lib/ffmpeg", params...)
+	cmd.Stdout = nil
+	cmd.Stderr = nil
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		util.Log().Error("Get ffmpeg stdin err:%v", err.Error())
+		return nil, nil, errors.New("拉流进程启动失败")
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		util.Log().Info("Start ffmpeg err: %v", err.Error())
+		return nil, nil, errors.New("打开摄像头视频流失败")
+	}
+	util.Log().Info("Translate rtsp %v to %v", rtsp, playCh)
+	return cmd, stdin, nil
+}
